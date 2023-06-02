84f030f92de8efc6134c4a5c0c2c1794a356120a
Sushank <128348117+isushank@users.noreply.github.com>
2023-06-02 13:55:40 +0530

    created simple bank db schema and added queries

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,42 +1,43 @@
-CREATE TABLE "account" (
-                           "id" bigserial PRIMARY KEY,
-                           "owner" varchar NOT NULL,
-                           "balance" varchar NOT NULL,
-                           "currency" varchar NOT NULL,
-                           "created_at" timestamptz NOT NULL DEFAULT (now())
+CREATE TABLE "accounts" (
+                            "id" bigserial PRIMARY KEY,
+                            "owner" varchar NOT NULL,
+                            "balance" bigint NOT NULL,
+                            "currency" varchar NOT NULL,
+                            "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "entries" (
                            "id" bigserial PRIMARY KEY,
-                           "account_id" bigint,
+                           "account_id" bigint NOT NULL,
                            "amount" bigint NOT NULL,
                            "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE TABLE "transfer" (
-                            "id" bigserial PRIMARY KEY,
-                            "from_account_id" bigint,
-                            "to_account_id" bigint,
-                            "amount" bigint NOT NULL,
-                            "created_at" timestamptz NOT NULL DEFAULT (now())
+CREATE TABLE "transfers" (
+                             "id" bigserial PRIMARY KEY,
+                             "from_account_id" bigint NOT NULL,
+                             "to_account_id" bigint NOT NULL,
+                             "amount" bigint NOT NULL,
+                             "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE INDEX ON "account" ("owner");
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+
+CREATE INDEX ON "accounts" ("owner");
 
 CREATE INDEX ON "entries" ("account_id");
 
-CREATE INDEX ON "transfer" ("from_account_id");
+CREATE INDEX ON "transfers" ("from_account_id");
 
-CREATE INDEX ON "transfer" ("to_account_id");
+CREATE INDEX ON "transfers" ("to_account_id");
 
-CREATE INDEX ON "transfer" ("from_account_id", "to_account_id");
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 
 COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
 
-COMMENT ON COLUMN "transfer"."amount" IS 'must be positive';
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
 
-ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "account" ("id");
-
-ALTER TABLE "transfer" ADD FOREIGN KEY ("from_account_id") REFERENCES "account" ("id");
-
-ALTER TABLE "transfer" ADD FOREIGN KEY ("to_account_id") REFERENCES "account" ("id");
